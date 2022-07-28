44eeb18a28538ce71cda886488cfda744a09395e
Braden Steffaniak <BradenSteffaniak@gmail.com>
2022-07-28 07:48:29 -0400

    Add basic inlining of array initializers functionality

--- src/org/flatlang/es6/nodewriters/AnonymousCompilerMethodWriter.java
+++ src/org/flatlang/es6/nodewriters/AnonymousCompilerMethodWriter.java
@@ -1,12 +1,22 @@
 package org.flatlang.es6.nodewriters;
 
+import org.flatlang.es6.engines.ES6CompileEngine;
 import org.flatlang.tree.*;
 
 public abstract class AnonymousCompilerMethodWriter extends BodyMethodDeclarationWriter
 {
 	public abstract AnonymousCompilerMethod node();
 
-//	@Override
+	@Override
+	public StringBuilder write(StringBuilder builder) {
+		if (ES6CompileEngine.INLINE_ARRAY_INITIALIZERS && node().getProperty("array") != null) {
+			return builder;
+		}
+
+		return super.write(builder);
+	}
+
+	//	@Override
 //	public StringBuilder writePrototypeAccess(StringBuilder builder) {
 //		if (node().isExtension()) {
 //			return builder;
